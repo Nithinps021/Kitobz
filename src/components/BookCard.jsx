f162794cd5058a895e7092c5d33263f46878f0d6
nithinps021 <nithinps021@gmail.com>
2020-04-03 22:53:05 +0530

    addded bookcard

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,13 +1,55 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-export class BookCard extends Component {
-    render() {
-        return (
-            <div> 
-                
-            </div>
-        )
-    }
+//material ui
+import withStyles from "@material-ui/core/styles/withStyles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+
+const style = {
+  card: {
+    display: "flex"
+  },
+  image: {
+      height:200,
+      objectFit:"cover"
+
+  },
+};
+
+class BookCard extends Component {
+  render() {
+    const { classes, bookinfo } = this.props;
+    const {
+      bookId,
+      bookname,
+      branch,
+      forsem,
+      imgURL,
+      price,
+      status
+    } = bookinfo;
+    return (
+      <Grid item xs={12} sm={6} md={4} lg={3}>
+        <Card id={bookId}>
+          <CardMedia image={imgURL} title="Book Image" className={classes.image}/>
+          <CardContent>
+          <Typography variant="h5">{bookname}</Typography>
+            <Grid container direction='row' spacing={1}>
+                <Grid item xs={12} >
+                <Typography variant="h5" >₹  {price}</Typography>
+                </Grid>
+                <Grid item xs={6}>
+                <Typography varient="body1">Status {status}</Typography>
+                </Grid>
+            </Grid>
+          </CardContent>
+        </Card>
+      </Grid>
+    );
+  }
 }
 
-export default BookCard;
+export default withStyles (style)(BookCard);
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,76 +1,85 @@
-import React, { Component } from 'react'
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
-import NavBar from '../components/NavBar.jsx';
-import '../css/home.css'
+import NavBar from "../components/NavBar.jsx";
+import "../css/home.css";
+import BookCard from "../components/BookCard";
 
-import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
-import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper'
+import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+import Grid from "@material-ui/core/Grid";
 
 const Theme = createMuiTheme({
-    palette: {
-        primary: {
-          main: '#323232',
-          contrastText:"#fff"
-        },
-        secondary: {
-          main: '#ff1e56',
-          contrastText:"#fff"
-        },
+  palette: {
+    primary: {
+      main: "#323232",
+      contrastText: "#fff"
     },
-    typography:{
-      useNextVarients:true
+    secondary: {
+      main: "#ff1e56",
+      contrastText: "#fff"
     }
-})
+  },
+  typography: {
+    useNextVarients: true
+  }
+});
 
- class home extends Component {
-
-  state ={
+class home extends Component {
+  state = {
+    Allbooks:[
+      {
+        bookId:1323,
+        bookname:"Linear Equation and Probability",
+        branch:"CSE",
+        forsem:"S4",
+        imgURL:" https://firebasestorage.googleapis.com/v0/b/fir-fbb84.appspot.com/o/995801706070.jpeg?alt=media",
+        price:200,
+        status:"Available"
+      }
+    ],
     allbooks:null,
-  }
+  };
 
-  componentDidMount(){
-    axios.get('/allbooks')
-    .then(res => {
-      console.log(res)
-      this.setState({
-        allbooks:res.data  
+  componentDidMount() {
+    axios
+      .get("/allbooks")
+      .then(res => {
+        console.log(res);
+        this.setState({
+          allbooks: res.data
+        });
       })
-    })
-    .catch(error =>{
-      console.log(error)
-    })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
-    render() {
-      let books = this.state.allbooks ? (
-        this.state.allbooks.map(ele => {
-          return(
-            <ul>
-              <h4>{ele.bookname}</h4>
-              <li>{ele.forsem}</li>
-              <li>{ele.username}</li>
-            </ul>  
-          )
-        })
-      ): <p>Loding</p> 
-        
+  render() {
+    // let books = this.state.allbooks ? (
+    //   this.state.allbooks.map(ele => <BookCard bookinfo={ele} />)
+    // ) : (
+    //   <p>Loding</p>
+    // );
+    let books = this.state.Allbooks.map(ele =><BookCard bookinfo={ele} />)
 
-      
-      return (
-            <div>
-              <MuiThemeProvider theme ={Theme}>
-                  <NavBar></NavBar>
-                  <div style={{marginTop:"7vw",paddingLeft:"5vw"}}>
-                  <Grid container spacing={3} direction="row" className="grid-container"> 
-                </Grid>
-                  </div>
-              </MuiThemeProvider>
-             
-            </div>
-        )
-    }
+    return (
+      <div>
+        <MuiThemeProvider theme={Theme}>
+          <NavBar></NavBar>
+          <div style={{ marginTop: "15vh", padding: "3vw" }}>
+            <Grid
+              container
+              spacing={3}
+              direction="row"
+              className="grid-container"
+            >
+                {books}
+            </Grid>
+          </div>
+        </MuiThemeProvider>
+      </div>
+    );
+  }
 }
 export default home;
