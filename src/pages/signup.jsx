44a5346389ff7d1db52b118b0a3da14de94ddbb1
nithinps021 <nithinps021@gmail.com>
2020-04-07 00:55:56 +0530

    modified signup page

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -7,20 +7,23 @@ import Grid from "@material-ui/core/Grid";
 import withStyles from "@material-ui/core/styles/withStyles";
 import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
-import MenuItem from '@material-ui/core/MenuItem';
-import Select from '@material-ui/core/Select';
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import Select from "@material-ui/core/Select";
 import Button from "@material-ui/core/Button";
 import CircularProgress from "@material-ui/core/CircularProgress";
 
 const styles = {
   form: {
-    textAlign: "center",
+    // textAlign: "center",
+    padding: 20,
   },
   heading: {
     marginTop: "5vh",
+    textAlign: "center",
   },
   textField: {
-    marginTop: 20,
+    marginTop: 0,
     color: "white",
   },
   buttonStyle: {
@@ -35,6 +38,15 @@ const styles = {
   },
   progress: {
     position: "absolute",
+  },
+  label: {
+    marginTop: 7,
+  },
+  selector: {
+    minWidth: 150,
+  },
+  ref: {
+    textAlign: "center",
   },
 };
 class Signup extends Component {
@@ -93,15 +105,15 @@ class Signup extends Component {
     console.log(errors);
     return (
       <div>
-        <Grid container className={classes.form} spacing={4}>
+        <Grid container className={classes.form}>
           <Grid item sm></Grid>
           <Grid item sm>
             <Typography variant="h3" className={classes.heading}>
               Sign Up
             </Typography>
             <form noValidate onSubmit={this.handleSubmit}>
-              <Grid container spacing={2} direction="column">
-                <Grid item xs>
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
                   <TextField
                     type="text"
                     id="username"
@@ -113,12 +125,12 @@ class Signup extends Component {
                     onChange={this.handleChange}
                     color="secondary"
                     size="medium"
-                    helperText={errors.username}
-                    error={errors.username ? true : false}
+                    helperText={errors.handle}
+                    error={errors.handle ? true : false}
                     fullWidth
                   />
                 </Grid>
-                <Grid item xs>
+                <Grid item xs={12}>
                   <TextField
                     type="email"
                     id="email"
@@ -135,47 +147,135 @@ class Signup extends Component {
                     fullWidth
                   />
                 </Grid>
-                <Grid item sm direction="row">
+                <Grid item xs={6}>
+                  <InputLabel id="branchlabel" className={classes.label}>
+                    Branch
+                  </InputLabel>
                   <Select
-                    label="Branch"
-                    id="branch"
-                    name='branch'
+                    labelId="branchlabel"
+                    name="branch"
                     value={this.state.branch}
                     onChange={this.handleChange}
+                    className={classes.selector}
+                    color="secondary"
                   >
                     <MenuItem value={"CSE"}>Computer Science</MenuItem>
-                    <MenuItem value={"EC"}>Electronics And Communication</MenuItem>
-                    <MenuItem value={"EEE"}>Electrical And Electronics</MenuItem>
+                    <MenuItem value={"EC"}>
+                      Electronics And Communication
+                    </MenuItem>
+                    <MenuItem value={"EEE"}>
+                      Electrical And Electronics
+                    </MenuItem>
                     <MenuItem value={"ME"}>Mechanical Engineering</MenuItem>
                     <MenuItem value={"CE"}>Civil Engineering</MenuItem>
                     <MenuItem value={"AE"}>Applied Electronics</MenuItem>
                     <MenuItem value={"IE"}>Industrial Engineering</MenuItem>
                     <MenuItem value={"ARCHI"}>Architecture</MenuItem>
                   </Select>
-
+                </Grid>
+                <Grid item xs={6}>
+                  <InputLabel htmlFor="semester" className={classes.label}>
+                    Semester
+                  </InputLabel>
                   <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
+                    label="branch"
+                    labelId="semester"
+                    id="Semester"
+                    name="sem"
                     value={this.state.sem}
                     onChange={this.handleChange}
+                    className={classes.selector}
+                    color="secondary"
                   >
-                    <MenuItem value={"S1"}>Ten</MenuItem>
-                    <MenuItem value={"S2"}>Twenty</MenuItem>
-                    <MenuItem value={"S3"}>Thirty</MenuItem>
-                    <MenuItem value={"S4"}>Ten</MenuItem>
-                    <MenuItem value={"S5"}>Twenty</MenuItem>
-                    <MenuItem value={"S6"}>Thirty</MenuItem>
-                    <MenuItem value={"S7"}>Ten</MenuItem>
-                    <MenuItem value={"S8"}>Twenty</MenuItem>
-                    <MenuItem value={"S9"}>Thirty</MenuItem>
+                    <MenuItem value={"S1"}>Semester 1</MenuItem>
+                    <MenuItem value={"S2"}>Semester 2</MenuItem>
+                    <MenuItem value={"S3"}>Semester 3</MenuItem>
+                    <MenuItem value={"S4"}>Semester 4</MenuItem>
+                    <MenuItem value={"S5"}>Semester 5</MenuItem>
+                    <MenuItem value={"S6"}>Semester 6</MenuItem>
+                    <MenuItem value={"S7"}>Semester 7</MenuItem>
+                    <MenuItem value={"S8"}>Semester 8</MenuItem>
+                    <MenuItem value={"S9"}>Semester 9</MenuItem>
+                    <MenuItem value={"S10"}>Semester 10</MenuItem>
                   </Select>
                 </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    type="number"
+                    id="phoneNo"
+                    name="phoneNo"
+                    label="Phone Number"
+                    key="PhoneNO"
+                    className={classes.textField}
+                    value={this.state.phoneNo}
+                    onChange={this.handleChange}
+                    color="secondary"
+                    size="medium"
+                    helperText={errors.phoneNo}
+                    error={errors.phoneNo ? true : false}
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    type="password"
+                    id="password"
+                    name="password"
+                    label="Password"
+                    key="password"
+                    className={classes.textField}
+                    value={this.state.password}
+                    onChange={this.handleChange}
+                    color="secondary"
+                    size="medium"
+                    helperText={errors.passwd}
+                    error={errors.passwd ? true : false}
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    type="confPasswd"
+                    id="confPasswd"
+                    name="confPasswd"
+                    label="Confirm Password"
+                    key="confPasswd"
+                    className={classes.textField}
+                    value={this.state.confPasswd}
+                    onChange={this.handleChange}
+                    color="secondary"
+                    size="medium"
+                    helperText={errors.confPasswd}
+                    error={errors.confpasswd ? true : false}
+                    fullWidth
+                  />
+                </Grid>
               </Grid>
+             <div className={classes.ref}>
+              <Button
+                  type="submit"
+                  color="secondary"
+                  variant="contained"
+                  className={classes.buttonStyle}
+                  disabled={loading}
+                >
+                  Signup
+                  {loading && (
+                    <CircularProgress
+                      className={classes.progress}
+                      color="secondary"
+                      size={30}
+                    />
+                  )}
+                </Button>
+             </div>
             </form>
             <br></br>
-            <small>
-              Dont have an account ? sign up <Link to="/signup">Here</Link>
-            </small>
+            <div className={classes.ref}>
+              <small>
+                Already have an account ? Login <Link to="/login">Here</Link>
+              </small>
+            </div>
           </Grid>
           <Grid item sm></Grid>
         </Grid>
