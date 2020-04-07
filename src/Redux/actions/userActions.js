4e8df7c2484ee7f6713efeb2d39a3da825f69948
nithinps021 <nithinps021@gmail.com>
2020-04-07 23:36:26 +0530

    added these components

---
+++ src/Redux/actions/userActions.js
@@ -0,0 +1,64 @@
+import {SET_ERRORS,LOADING_UI,CLEAR_ERRORS,SET_UNAUTHENTICATED} from '../type'
+import axios from 'axios'
+
+export const loginUser = (loginDetails, history) => (dispach) =>{
+    dispach({type:LOADING_UI})
+    axios
+      .post("/login", loginDetails)
+      .then((res) => {
+        const FBIdToken = `Bearer ${res.data.token}`
+        localStorage.setItem('FBToken',FBIdToken);
+        axios.defaults.headers.common['Authorization']=FBIdToken;
+        // dispach(getUserData())
+        dispach({type:CLEAR_ERRORS})
+        history.push("/");
+      })
+      .catch((error) => {
+          dispach({
+              type:SET_ERRORS,
+              payload:error.response.data
+          })
+        console.log(error.response.data)
+      });
+}
+
+// export const getUserData = () => (dispatch)=>{
+//         axios.get('/user')
+//             .then(res =>{
+//                 dispatch({
+//                    type:SET_USER,
+//                    payload:res.data 
+//                 })
+//             })
+//             .catch(err =>{
+//                 console.log(err);
+//             })
+// }
+
+export const signupUser = (signDetails, history) => (dispach) =>{
+  dispach({type:LOADING_UI})
+  axios
+    .post("/signup", signDetails)
+    .then((res) => {
+      console.log(res.data);
+      const FBIdToken = `Bearer ${res.data.token}`
+      localStorage.setItem('FBToken',FBIdToken);
+      axios.defaults.headers.common['Authorization']=FBIdToken;
+      // dispach(getUserData())
+      dispach({type:CLEAR_ERRORS})
+      history.push("/");
+    })
+    .catch((error) => {
+        dispach({
+            type:SET_ERRORS,
+            payload:error.response.data
+        })
+      console.log(error.response.data)
+    });
+}
+
+export const logoutUser = () => (dispach) =>{
+  localStorage.removeItem('FBToken');
+  delete axios.defaults.headers.common['Authorization']
+  dispach({type:SET_UNAUTHENTICATED})
+}
---
+++ src/util/homeRoute.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+
+const homeRoute = ({ component: Componenet, authenticated, ...rest }) => (
+  <Route
+    {...rest}
+    render={(props) =>
+      authenticated === true ? <Componenet {...props} /> : <Redirect to="/login" />
+    }
+  />
+);
+const mapStateToProp = (state) => ({
+  authenticated: state.user.authentication,
+});
+
+export default connect(mapStateToProp)(homeRoute);
