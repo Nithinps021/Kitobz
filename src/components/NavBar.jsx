cb0866eb5498d006ced5be434ff52c4e11049828
nithinps021 <nithinps021@gmail.com>
2020-04-18 17:45:00 +0530

    added drawer

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component} from "react";
 
 // components import
 import AddBook from "../components/AddBook";
@@ -7,40 +7,189 @@ import Logout from '../components/Logout'
 //material ui imports
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import { Button } from "@material-ui/core";
-import KeyboardReturnIcon from "@material-ui/icons/KeyboardReturn";
 import Tooltip from "@material-ui/core/Tooltip";
 import IconButton from "@material-ui/core/IconButton";
 import HomeIcon from "@material-ui/icons/Home";
+import Grid from '@material-ui/core/Grid'
+
+// drawer
+import clsx from "clsx";
+import { withStyles, makeStyles, useTheme } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import List from "@material-ui/core/List";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import MenuIcon from "@material-ui/icons/Menu";
+import CloseIcon from "@material-ui/icons/Close";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import MailIcon from "@material-ui/icons/Mail";
 
 
+const drawerWidth = 220;
+
+const useStyles = (theme) => ({
+  root: {
+    display: "flex",
+  },
+  appBar: {
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  hide: {
+    display: "none",
+  },
+  drawer: {
+    marginTop: 50,
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+    backgroundColor: "#323232",
+    color:"white"
+  },
+  drawerHeader: {
+    display: "flex",
+    alignItems: "center",
+    padding: theme.spacing(0, 6),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: "flex-end",
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  },
+});
 
 export class NavBar extends Component {
   constructor(props) {
     super(props);
   }
+  state = {
+    open: false,
+  };
+  handleOpen = () => {
+    this.setState({
+      open: true,
+    });
+  };
+  handleClose = () => {
+    this.setState({
+      open: false,
+    });
+  };
   hadndleLogout = () => {
     this.props.logoutUser();
   };
   render() {
+     const { classes } = this.props;
+    
     return (
       <div>
-        <AppBar>
-          <Toolbar className="nav-container">
-            <Tooltip title="Home">
-              <IconButton>
-                <HomeIcon color="secondary"></HomeIcon>
-              </IconButton>
-            </Tooltip>
-            <AddBook></AddBook>
-            <Logout></Logout>
-          </Toolbar>
+        <AppBar
+  
+        >
+          <Grid container>
+            <Grid item xs={6} lg={4}>
+              <Toolbar>
+                <IconButton
+                  color="inherit"
+                  aria-label="open drawer"
+                  onClick={this.handleOpen}
+                  edge="start"
+                  className={clsx(
+                    classes.menuButton,
+                    this.state.open && classes.hide
+                  )}
+                >
+                  <MenuIcon color="secondary"/>
+                </IconButton>
+              </Toolbar>
+            </Grid>
+            <Grid item lg={6}></ Grid>
+            <Grid item xs={6} lg={2}>
+              <Toolbar>
+                <Tooltip title="Home">
+                  <IconButton>
+                    <HomeIcon color="secondary"></HomeIcon>
+                  </IconButton>
+                </Tooltip>
+                <AddBook></AddBook>
+                <Logout></Logout>
+              </Toolbar>
+            </Grid>
+          </Grid>
         </AppBar>
+        <Drawer
+          className={classes.drawer}
+          variant="persistent"
+          anchor="left"
+          open={this.state.open}
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+        >
+          <div className={classes.drawerHeader}>
+            <IconButton onClick={this.handleClose}>
+              <CloseIcon color="secondary"/>
+            </IconButton>
+          </div>
+          <Divider />
+          <List>
+            {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+          <List>
+            {["All mail", "Trash", "Spam"].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon color="secondary"/> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+        </Drawer>
       </div>
     );
   }
 }
 
-
-export default NavBar;
+export default withStyles(useStyles)(NavBar);
