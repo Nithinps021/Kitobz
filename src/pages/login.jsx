a061e10ec605d827276e74adbdca1e57850f8852
nithinps021 <nithinps021@gmail.com>
2020-04-25 22:01:35 +0530

    made as dialog element

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
 // MUI imports
@@ -9,21 +8,43 @@ import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import CircularProgress from "@material-ui/core/CircularProgress";
+import Dialog from "@material-ui/core/Dialog";
+import DialogContent from "@material-ui/core/DialogContent";
 
 // redux imports
 import { connect } from "react-redux";
 import { loginUser } from "../Redux/actions/userActions";
 
 const styles = {
+  root: {
+    // background: "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(0,0,0,1) 0%, rgba(204,8,8,1) 0%, rgba(248,27,76,1) 87%, rgba(255,30,86,1) 100%)",
+    borderRadius: 15,
+    border: 0,
+    color: "white",
+    height: 30,
+    padding: "2px 25px",
+    background:
+      "linear-gradient(0deg, rgba(50,50,50,1) 0%, rgba(255,30,86,1) 100%)",
+  },
+  label: {
+    textTransform: "capitalize",
+  },
+  labelbutton: {
+    textTransform: "capitalize",
+    color: "#ff1e56",
+  },
   form: {
     textAlign: "center",
   },
   heading: {
-    marginTop: "5vh",
+    marginTop: "3vh",
   },
   textField: {
-    marginTop: 30,
+    marginTop: 25,
     color: "white",
+    "& label": {
+      color: "black",
+    },
   },
   buttonStyle: {
     marginTop: "4vh",
@@ -38,6 +59,9 @@ const styles = {
   progress: {
     position: "absolute",
   },
+  dialog: {
+    Bottom: "10vh",
+  },
 };
 
 class login extends Component {
@@ -47,6 +71,7 @@ class login extends Component {
       email: "",
       password: "",
       errors: {},
+      open: false,
     };
   }
   componentWillReceiveProps(nextProps) {
@@ -56,6 +81,16 @@ class login extends Component {
       });
     }
   }
+  handleOpen = () => {
+    this.setState({
+      open: true,
+    });
+  };
+  handleClose = () => {
+    this.setState({
+      open: false,
+    });
+  };
   handleSubmit = (event) => {
     event.preventDefault();
     const loginDetails = {
@@ -72,79 +107,105 @@ class login extends Component {
   render() {
     const {
       classes,
-      ui: { loading},
+      ui: { loading },
     } = this.props;
     const { errors } = this.state;
     console.log(errors);
     return (
-      <div>
-        <Grid container className={classes.form} >
-          <Grid item sm></Grid>
-          <Grid item sm>
-            <Typography variant="h3" className={classes.heading}>
-              Login
-            </Typography>
-            <form noValidate onSubmit={this.handleSubmit}>
-              <TextField
-                type="email"
-                id="email"
-                name="email"
-                label="Email"
-                key="email"
-                className={classes.textField}
-                value={this.state.email}
-                onChange={this.handleChange}
-                color="secondary"
-                size="medium"
-                helperText={errors.email}
-                error={errors.email ? true : false}
-                fullWidth
-              />
-
-              <TextField
-                type="password"
-                id="password"
-                name="password"
-                label="Password"
-                key="password"
-                className={classes.textField}
-                value={this.state.password}
-                onChange={this.handleChange}
-                color="secondary"
-                size="medium"
-                helperText={errors.passwd}
-                error={errors.email ? true : false}
-                fullWidth
-              />
-              {errors.Error && (
-                <Typography className={classes.errorText} variant="body2">
-                  {errors.Error}
-                </Typography>
-              )}
-              <Button
-                type="submit"
-                color="secondary"
-                variant="contained"
-                className={classes.buttonStyle}
-                disabled={loading}
-              >
+      <div className={classes.dialog}>
+        <Button
+          variant="contained"
+          size="small"
+          onClick={this.handleOpen}
+          classes={{
+            root: classes.root,
+            label: classes.label,
+          }}
+        >
+          Login
+        </Button>
+        <Dialog
+          open={this.state.open}
+          onClose={this.handleClose}
+          maxWidth="sm"
+          fullWidth
+          className={classes.dialog}
+        >
+          <Grid container className={classes.form}>
+            <Grid item lg={2} xs={1}></Grid>
+            <Grid item lg={8} xs={10}>
+              <Typography variant="h3" className={classes.heading}>
                 Login
-                {loading && (
-                  <CircularProgress
-                    className={classes.progress}
-                    color="secondary"
-                    size={30}
-                  />
+              </Typography>
+              <form noValidate onSubmit={this.handleSubmit}>
+                <TextField
+                  type="email"
+                  id="email"
+                  name="email"
+                  label="Email"
+                  key="email"
+                  className={classes.textField}
+                  value={this.state.email}
+                  onChange={this.handleChange}
+                  color="secondary"
+                  size="medium"
+                  helperText={errors.email}
+                  error={errors.email ? true : false}
+                  fullWidth
+                />
+
+                <TextField
+                  type="password"
+                  id="password"
+                  name="password"
+                  label="Password"
+                  key="password"
+                  className={classes.textField}
+                  value={this.state.password}
+                  onChange={this.handleChange}
+                  color="secondary"
+                  size="medium"
+                  helperText={errors.passwd}
+                  error={errors.email ? true : false}
+                  fullWidth
+                />
+                {errors.Error && (
+                  <Typography className={classes.errorText} variant="body2">
+                    {errors.Error}
+                  </Typography>
                 )}
-              </Button>
-            </form>
-            <br></br>
-            <small>
-              Dont have an account ? sign up <Link to="/signup">Here</Link>
-            </small>
+                <Button
+                  type="submit"
+                  color="secondary"
+                  variant="contained"
+                  className={classes.buttonStyle}
+                  disabled={loading}
+                >
+                  Login
+                  {loading && (
+                    <CircularProgress
+                      className={classes.progress}
+                      color="secondary"
+                      size={30}
+                    />
+                  )}
+                </Button>
+              </form>
+              <DialogContent className={classes.dialog}>
+                <small>
+                  Dont have an account ? sign up{" "}
+                  <Button onClick={this.handleClose} 
+                  classes={{
+                    label:classes.labelbutton
+                  }}>
+                    here
+                  </Button>
+                </small>
+              </DialogContent>
+            </Grid>
+            <Grid item lg={2} xs={1}></Grid>
           </Grid>
-          <Grid item sm></Grid>
-        </Grid>
+        </Dialog>
       </div>
     );
   }
--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
 // MUI imports
@@ -12,23 +11,59 @@ import MenuItem from "@material-ui/core/MenuItem";
 import Select from "@material-ui/core/Select";
 import Button from "@material-ui/core/Button";
 import CircularProgress from "@material-ui/core/CircularProgress";
+import Dialog from "@material-ui/core/Dialog";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+
 
 // Redux imports
-import {signupUser} from '../Redux/actions/userActions'
-import {connect} from 'react-redux'
+import { signupUser } from "../Redux/actions/userActions";
+import { connect } from "react-redux";
 
 const styles = {
+  "@global": {
+    "*::-webkit-scrollbar": {
+      width: "0.4em",
+    },
+    "*::-webkit-scrollbar-track": {
+      "-webkit-box-shadow": "inset 0 0 6px rgba(255,255,255,0.00)",
+    },
+    "*::-webkit-scrollbar-thumb": {
+      backgroundColor: "rgba(0,0,0,.1)",
+      outline: "1px solid white",
+    },
+  },
+  signupButton: {
+    borderRadius: 30,
+    border: 0,
+    color: "white",
+    height: 40,
+    padding: "6px 30px",
+    background:
+      "linear-gradient(0deg, rgba(50,50,50,1) 0%, rgba(255,30,86,1) 100%)",
+  },
+  label: {
+    textTransform: "capitalize",
+  },
+  labelbutton: {
+    textTransform: "capitalize",
+    color: "#ff1e56",
+  },
   form: {
     // textAlign: "center",
-    padding: 20,
+    // paddingLeft:35
   },
   heading: {
-    marginTop: "5vh",
+    // marginTop: "5vh",
     textAlign: "center",
   },
   textField: {
     marginTop: 0,
     color: "white",
+    "& label": {
+      color: "black",
+    },
   },
   buttonStyle: {
     marginTop: "4vh",
@@ -43,11 +78,16 @@ const styles = {
   progress: {
     position: "absolute",
   },
-  label: {
+  labelSelect: {
     marginTop: 7,
+    color: "black",
   },
   selector: {
     minWidth: 150,
+  },
+  action: {
+    alignSelf: "center",
+    justifyContent: "center",
   },
   ref: {
     textAlign: "center",
@@ -65,13 +105,24 @@ class Signup extends Component {
       branch: "",
       sem: "",
       errors: {},
+      open: false,
     };
   }
-  componentWillReceiveProps(nextProps){
-    if(nextProps.ui.errors){
+  handleOpen = () => {
+    this.setState({
+      open: true,
+    });
+  };
+  handleClose = () => {
+    this.setState({
+      open: false,
+    });
+  };
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.ui.errors) {
       this.setState({
-        errors:nextProps.ui.errors
-      })
+        errors: nextProps.ui.errors,
+      });
     }
   }
   handleSubmit = (event) => {
@@ -88,8 +139,8 @@ class Signup extends Component {
       phoneNo: this.state.phoneNo,
       sem: this.state.sem,
     };
-    console.log(loginDetails)
-    this.props.signupUser(loginDetails,this.props.history)
+    console.log(loginDetails);
+    this.props.signupUser(loginDetails, this.props.history);
   };
   handleChange = (event) => {
     this.setState({
@@ -97,202 +148,240 @@ class Signup extends Component {
     });
   };
   render() {
-    const { classes,ui:{loading} } = this.props;
+    const {
+      classes,
+      ui: { loading },
+    } = this.props;
     const { errors } = this.state;
     console.log(errors);
     return (
       <div>
-        <Grid container className={classes.form}>
-          <Grid item sm></Grid>
-          <Grid item sm>
-            <Typography variant="h3" className={classes.heading}>
-              Sign Up
-            </Typography>
-            <form noValidate onSubmit={this.handleSubmit}>
-              <Grid container spacing={2}>
-                <Grid item xs={12}>
-                  <TextField
-                    type="text"
-                    id="username"
-                    name="username"
-                    label="Username"
-                    key="username"
-                    className={classes.textField}
-                    value={this.state.username}
-                    onChange={this.handleChange}
-                    color="secondary"
-                    size="medium"
-                    helperText={errors.handle}
-                    error={errors.handle ? true : false}
-                    fullWidth
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    type="text"
-                    id="email"
-                    name="email"
-                    label="Email"
-                    key="email"
-                    className={classes.textField}
-                    value={this.state.email}
-                    onChange={this.handleChange}
-                    color="secondary"
-                    size="medium"
-                    helperText={errors.email}
-                    error={errors.email ? true : false}
-                    fullWidth
-                  />
-                </Grid>
-                <Grid item xs={6}>
-                  <InputLabel id="branchlabel" className={classes.label}>
-                    Branch
-                  </InputLabel>
-                  <Select
-                    labelId="branchlabel"
-                    name="branch"
-                    value={this.state.branch}
-                    onChange={this.handleChange}
-                    className={classes.selector}
-                    color="secondary"
-                  >
-                    <MenuItem value={"CSE"}>Computer Science</MenuItem>
-                    <MenuItem value={"EC"}>
-                      Electronics And Communication
-                    </MenuItem>
-                    <MenuItem value={"EEE"}>
-                      Electrical And Electronics
-                    </MenuItem>
-                    <MenuItem value={"ME"}>Mechanical Engineering</MenuItem>
-                    <MenuItem value={"CE"}>Civil Engineering</MenuItem>
-                    <MenuItem value={"AE"}>Applied Electronics</MenuItem>
-                    <MenuItem value={"IE"}>Industrial Engineering</MenuItem>
-                    <MenuItem value={"ARCHI"}>Architecture</MenuItem>
-                  </Select>
-                </Grid>
-                <Grid item xs={6}>
-                  <InputLabel htmlFor="semester" className={classes.label}>
-                    Semester
-                  </InputLabel>
-                  <Select
-                    label="branch"
-                    labelId="semester"
-                    id="Semester"
-                    name="sem"
-                    value={this.state.sem}
-                    onChange={this.handleChange}
-                    className={classes.selector}
-                    color="secondary"
-                  >
-                    <MenuItem value={"S1"}>Semester 1</MenuItem>
-                    <MenuItem value={"S2"}>Semester 2</MenuItem>
-                    <MenuItem value={"S3"}>Semester 3</MenuItem>
-                    <MenuItem value={"S4"}>Semester 4</MenuItem>
-                    <MenuItem value={"S5"}>Semester 5</MenuItem>
-                    <MenuItem value={"S6"}>Semester 6</MenuItem>
-                    <MenuItem value={"S7"}>Semester 7</MenuItem>
-                    <MenuItem value={"S8"}>Semester 8</MenuItem>
-                    <MenuItem value={"S9"}>Semester 9</MenuItem>
-                    <MenuItem value={"S10"}>Semester 10</MenuItem>
-                  </Select>
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    type="number"
-                    id="phoneNo"
-                    name="phoneNo"
-                    label="Phone Number"
-                    key="PhoneNO"
-                    className={classes.textField}
-                    value={this.state.phoneNo}
-                    onChange={this.handleChange}
-                    color="secondary"
-                    size="medium"
-                    helperText={errors.phoneNo}
-                    error={errors.phoneNo ? true : false}
-                    fullWidth
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    type="password"
-                    id="password"
-                    name="password"
-                    label="Password"
-                    key="password"
-                    className={classes.textField}
-                    value={this.state.password}
-                    onChange={this.handleChange}
-                    color="secondary"
-                    size="medium"
-                    helperText={errors.passwd}
-                    error={errors.passwd ? true : false}
-                    fullWidth
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    type="password"
-                    id="confPasswd"
-                    name="confPasswd"
-                    label="Confirm Password"
-                    key="confPasswd"
-                    className={classes.textField}
-                    value={this.state.confPasswd}
-                    onChange={this.handleChange}
-                    color="secondary"
-                    size="medium"
-                    helperText={errors.confpasswd}
-                    error={errors.confpasswd ? true : false}
-                    fullWidth
-                  />
-                </Grid>
-              </Grid>
-             <div className={classes.ref}>
-              <Button
-                  type="submit"
-                  color="secondary"
-                  variant="contained"
-                  className={classes.buttonStyle}
-                  disabled={loading}
-                >
-                  Signup
-                  {loading && (
-                    <CircularProgress
-                      className={classes.progress}
+        <Button
+          variant="contained"
+          onClick={this.handleOpen}
+          size="small"
+          classes={{
+            root: classes.signupButton,
+            label: classes.label,
+          }}
+        >
+          Signup
+        </Button>
+        <Dialog
+          open={this.state.open}
+          onClose={this.handleClose}
+          maxWidth="sm"
+          fullWidth
+        >
+          <DialogTitle className={classes.heading}>
+            <Typography variant="h4">Signup</Typography>
+          </DialogTitle>
+          <Grid container className={classes.form}>
+            <Grid item lg={1} xs={1}></Grid>
+            <Grid item lg={10} xs={10}>
+              <form noValidate onSubmit={this.handleSubmit}>
+                <Grid container spacing={1}>
+                  <Grid item xs={10} lg={10}>
+                    <TextField
+                      type="text"
+                      id="username"
+                      name="username"
+                      label="Username"
+                      key="username"
+                      className={classes.textField}
+                      value={this.state.username}
+                      onChange={this.handleChange}
                       color="secondary"
-                      size={30}
+                      size="medium"
+                      helperText={errors.handle}
+                      error={errors.handle ? true : false}
+                      fullWidth
                     />
-                  )}
-                </Button>
-             </div>
-            </form>
-            <br></br>
-            <div className={classes.ref}>
-              <small>
-                Already have an account ? Login <Link to="/login">Here</Link>
-              </small>
-            </div>
+                  </Grid>
+                  <Grid item xs={10} lg={10}>
+                    <TextField
+                      type="text"
+                      id="email"
+                      name="email"
+                      label="Email"
+                      key="email"
+                      className={classes.textField}
+                      value={this.state.email}
+                      onChange={this.handleChange}
+                      color="secondary"
+                      size="medium"
+                      helperText={errors.email}
+                      error={errors.email ? true : false}
+                      fullWidth
+                    />
+                  </Grid>
+                  <Grid item xs={10} lg={10}>
+                    <InputLabel
+                      id="branchlabel"
+                      className={classes.labelSelect}
+                    >
+                      Branch
+                    </InputLabel>
+                    <Select
+                      labelId="branchlabel"
+                      name="branch"
+                      value={this.state.branch}
+                      onChange={this.handleChange}
+                      className={classes.selector}
+                      color="secondary"
+                    >
+                      <MenuItem value={"CSE"}>Computer Science</MenuItem>
+                      <MenuItem value={"EC"}>
+                        Electronics And Communication
+                      </MenuItem>
+                      <MenuItem value={"EEE"}>
+                        Electrical And Electronics
+                      </MenuItem>
+                      <MenuItem value={"ME"}>Mechanical Engineering</MenuItem>
+                      <MenuItem value={"CE"}>Civil Engineering</MenuItem>
+                      <MenuItem value={"AE"}>Applied Electronics</MenuItem>
+                      <MenuItem value={"IE"}>Industrial Engineering</MenuItem>
+                      <MenuItem value={"ARCHI"}>Architecture</MenuItem>
+                    </Select>
+                  </Grid>
+                  <Grid item xs={10} lg={10}>
+                    <InputLabel
+                      htmlFor="semester"
+                      className={classes.labelSelect}
+                    >
+                      Semester
+                    </InputLabel>
+                    <Select
+                      label="branch"
+                      labelId="semester"
+                      id="Semester"
+                      name="sem"
+                      value={this.state.sem}
+                      onChange={this.handleChange}
+                      className={classes.selector}
+                      color="secondary"
+                    >
+                      <MenuItem value={"S1"}>Semester 1</MenuItem>
+                      <MenuItem value={"S2"}>Semester 2</MenuItem>
+                      <MenuItem value={"S3"}>Semester 3</MenuItem>
+                      <MenuItem value={"S4"}>Semester 4</MenuItem>
+                      <MenuItem value={"S5"}>Semester 5</MenuItem>
+                      <MenuItem value={"S6"}>Semester 6</MenuItem>
+                      <MenuItem value={"S7"}>Semester 7</MenuItem>
+                      <MenuItem value={"S8"}>Semester 8</MenuItem>
+                      <MenuItem value={"S9"}>Semester 9</MenuItem>
+                      <MenuItem value={"S10"}>Semester 10</MenuItem>
+                    </Select>
+                  </Grid>
+                  <Grid item xs={10} lg={10}>
+                    <TextField
+                      type="number"
+                      id="phoneNo"
+                      name="phoneNo"
+                      label="Phone Number"
+                      key="PhoneNO"
+                      className={classes.textField}
+                      value={this.state.phoneNo}
+                      onChange={this.handleChange}
+                      color="secondary"
+                      size="medium"
+                      helperText={errors.phoneNo}
+                      error={errors.phoneNo ? true : false}
+                      fullWidth
+                    />
+                  </Grid>
+                  <Grid item xs={10} lg={10}>
+                    <TextField
+                      type="password"
+                      id="password"
+                      name="password"
+                      label="Password"
+                      key="password"
+                      className={classes.textField}
+                      value={this.state.password}
+                      onChange={this.handleChange}
+                      color="secondary"
+                      size="medium"
+                      helperText={errors.passwd}
+                      error={errors.passwd ? true : false}
+                      fullWidth
+                    />
+                  </Grid>
+                  <Grid item xs={10} lg={10}>
+                    <TextField
+                      type="password"
+                      id="confPasswd"
+                      name="confPasswd"
+                      label="Confirm Password"
+                      key="confPasswd"
+                      className={classes.textField}
+                      value={this.state.confPasswd}
+                      onChange={this.handleChange}
+                      color="secondary"
+                      size="medium"
+                      helperText={errors.confpasswd}
+                      error={errors.confpasswd ? true : false}
+                      fullWidth
+                    />
+                  </Grid>
+                </Grid>
+                <DialogActions className={classes.action}>
+                  <Button
+                    type="submit"
+                    color="secondary"
+                    variant="contained"
+                    className={classes.buttonStyle}
+                    disabled={loading}
+                  >
+                    Signup
+                    {loading && (
+                      <CircularProgress
+                        className={classes.progress}
+                        color="secondary"
+                        size={30}
+                      />
+                    )}
+                  </Button>
+                </DialogActions>
+              </form>
+              <br></br>
+              <DialogContent className={classes.ref}>
+                <small>
+                  Already have an account ? Login{" "}
+                  <Button
+                    onClick={this.handleClose}
+                    classes={{
+                      label: classes.labelbutton,
+                    }}
+                  >
+                    here
+                  </Button>
+                </small>
+              </DialogContent>
+            </Grid>
+            <Grid item xs={1} lg={1}></Grid>
           </Grid>
-          <Grid item sm></Grid>
-        </Grid>
+        </Dialog>
       </div>
     );
   }
 }
 Signup.propType = {
-  classes : PropTypes.object.isRequired,
-  ui:PropTypes.object.isRequired,
-  user:PropTypes.object.isRequired,
-  signupUser:PropTypes.func.isRequired
-}
+  classes: PropTypes.object.isRequired,
+  ui: PropTypes.object.isRequired,
+  user: PropTypes.object.isRequired,
+  signupUser: PropTypes.func.isRequired,
+};
 
-const mapStateToProp =(state) =>({
-  ui:state.ui,
-  user:state.user
-})
-const mapActionToProp ={
-  signupUser
-}
+const mapStateToProp = (state) => ({
+  ui: state.ui,
+  user: state.user,
+});
+const mapActionToProp = {
+  signupUser,
+};
 
-export default connect(mapStateToProp,mapActionToProp)(withStyles(styles)(Signup))
+export default connect(
+  mapStateToProp,
+  mapActionToProp
+)(withStyles(styles)(Signup));
